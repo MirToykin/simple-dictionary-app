5950beb8dbad4d96bb5f6e06dacb79407dc2800b
MirToykin <miroslav.toykin@gmail.com>
2021-03-01 21:08:25 +0300

    сделана заглушка экрана профиля

--- src/screens/authScreens/ProfileScreen.tsx
+++ src/screens/authScreens/ProfileScreen.tsx
@@ -1,34 +1,83 @@
-import React from 'react';
-import {Text, TextInput, TouchableOpacity, View} from "react-native";
+import React, {useEffect} from 'react';
+import {Image, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
 import {ThunkDispatch} from "redux-thunk";
 import {AppStateType} from "../../redux/store/configureStore";
 import {AuthActionType, logout} from "../../redux/actions/authActions";
 import {useDispatch, useSelector} from "react-redux";
 import AppHeader from "../../components/AppHeader";
+//@ts-ignore
+import UserPhoto from '../../assets/images/user.png'
 
 const ProfileScreen = () => {
   const thunkDispatch: ThunkDispatch<AppStateType, unknown, AuthActionType> = useDispatch()
   const options = useSelector((state: AppStateType) => state.auth.options)
+  const {name, email} = useSelector((state: AppStateType) => state.auth)
 
   const handleLogout = () => {
     thunkDispatch(logout(options))
   }
 
+  const renderRow = (cells: any) => {
+    return cells.map((cell: any) => (
+      <View style={styles.cell} key={cell.title}>
+        <Text style={styles.cellTitle}>{cell.title}</Text>
+        <Text style={styles.cellValue}>{cell.value}</Text>
+      </View>
+    ));
+  };
+
   return (
     <>
       <AppHeader/>
-      <View style={{
-        flex:1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
-        <Text>Профиль</Text>
-        <TouchableOpacity onPress={handleLogout}>
-          <Text>Выход</Text>
+      <View style={styles.container}>
+        <Image
+          source={UserPhoto}
+          style={styles.avatar}
+          resizeMode={'contain'}
+        />
+        {renderRow([
+          {title: 'Имя', value: name},
+          {title: 'E-mail', value: email}
+        ])}
+        <TouchableOpacity style={styles.logout} onPress={handleLogout}>
+          <Text style={styles.logoutText}>Выход</Text>
         </TouchableOpacity>
       </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginTop: 20,
+  },
+  cell: {
+    marginTop: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cellTitle: {
+    fontSize: 13,
+    color: '#b0b0b0',
+  },
+  cellValue: {
+    marginTop: 10,
+    fontSize: 16,
+    color: '#2e2e2e',
+  },
+  logout: {
+    marginTop: 15,
+  },
+  logoutText: {
+    fontWeight: 'bold'
+  }
+})
 
 export default ProfileScreen;
